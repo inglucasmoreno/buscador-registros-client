d993dcba36b1cecc8eb38cf0ffa23e2910cc419b
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2023-03-08 14:56:21 -0300

    Inicio de proyecto

---
+++ src/app/pages/buscador/buscador.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/services/alert.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataService } from 'src/app/services/data.service';
+import gsap from 'gsap';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styles: [
+  ]
+})
+export class BuscadorComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService,
+    private alertService: AlertService,
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(): void {
+    gsap.from('.gsap-contenido', { y:100, opacity: 0, duration: .2 });
+    this.dataService.ubicacionActual = 'Dashboard - Buscador';
+  }
+
+  buscarRegistro(): void {
+
+  }
+
+}
